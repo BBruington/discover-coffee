1e130b9a2a512db5893be127efc043354beb68eb
BBruington <Psychological_Chemist@hotmail.com>
2022-06-22 21:11:25 -0400

    Coffee App Start

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return(
+    <>
+      <Component {...pageProps} />
+      <footer>
+        <p>This is a footer</p>
+      </footer>
+    </>
+  )
 }
 
 export default MyApp
