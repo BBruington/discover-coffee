4058bad09f70f7c1312c3b04b4c0c41d7a9a2ba2
BBruington <Psychological_Chemist@hotmail.com>
2022-07-05 16:42:46 -0400

    updating createCoffeeStore api

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -8,12 +8,53 @@ const table = base('coffee-stores');
 
 console.log({ table });
 
-const createCoffeeStore = (req, res) => {
-  if (req.method === 'POST'){
-    res.json({message: "This is the createCoffeeStore api POST"})
-  } else {
-    res.json({ message: 'This is the createCoffeeStore api GET'})
+const createCoffeeStore = async (req, res) => {
+  if (req.method === "POST") {
+    //find a record
+
+    const { id, name, neighborhood, address, imgUrl, voting } = req.body;
+
+    try {
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id=${id}`,
+        })
+        .firstPage();
+
+      console.log({ findCoffeeStoreRecords });
+      if (findCoffeeStoreRecords.length !== 0) {
+        const records = findCoffeeStoreRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+        res.json(records);
+      } else {
+        //create a record
+        const createRecords = await table.create([
+          {
+            fields: {
+              id,
+              name,
+              address,
+              neighborhood,
+              voting,
+              imgUrl,
+            },
+          },
+        ]);
+        const records = createRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+        res.json(records);
+      }
+    } catch (err) {
+      console.error("Error finding store", err);
+      res.status(500);
+      res.json({ message: "Error finding store", err });
+    }
   }
 };
-
 export default createCoffeeStore;
